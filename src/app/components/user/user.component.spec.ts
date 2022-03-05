ab54ef43d3b791fcfe5d381fe903f8205e2e6f0b
genka <tamani.coding@gmail.com>
2022-03-05 23:33:08 +0100

    user component test

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -1,3 +1,5 @@
+import { Observable, of } from 'rxjs';
+import { UserService } from './../../services/user.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
@@ -7,10 +9,32 @@ describe('UserComponent', () => {
   let fixture: ComponentFixture<UserComponent>;
 
   beforeEach(async () => {
+    const userServiceMock = jasmine.createSpyObj<UserService>(['getUser']);
+    // mock user service function
+    userServiceMock.getUser.and.callFake(function () {
+      return of({
+        results: [
+          {
+            name: {
+              title: 'Mr',
+              first: 'Peter',
+              last: 'Parker'
+            }
+          }
+        ]
+      });
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
+      declarations: [UserComponent],
+      providers: [
+        {
+          provide: UserService,
+          useValue: userServiceMock
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
